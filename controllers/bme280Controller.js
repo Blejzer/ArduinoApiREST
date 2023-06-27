8de0f5287e3388cb687914d236fbb70f6b35d1ec
Blejzer <nikola.kujaca@gmail.com>
2023-06-27 13:31:46 +0200

    Small update/upgrade

--- controllers/bme280Controller.js
+++ controllers/bme280Controller.js
@@ -7,12 +7,30 @@ const  Data = require("../models/dataModel");
 exports.indexBme280 = (req, res) => {
 
     Data.findOne({}, {}, { sort: { 'date' : -1 } }, function(err, data) {
-        data.date  = data.date.toLocaleString('hr-HR');
-        data.humidity = data.humidity.toFixed(2);
-        data.temperature = data.temperature.toFixed(2);
-        data.pressure = data.pressure.toFixed(2);
-        console.log( data );
-        res.render('indexbme280', { data });
+        try {
+            data.date  = data.date.toLocaleString('hr-HR');
+            data.humidity = data.humidity.toFixed(2);
+            data.temperature = data.temperature.toFixed(2);
+            data.pressure = data.pressure.toFixed(2);
+            console.log( data );
+            res.render('indexbme280', { data });
+            if (err) {
+                console.log('Doslo je do greske.')
+                console.error(err)
+                res.status(501).send(err)
+            }else {
+                res.status(200).json(data)
+            }
+        } catch (err) {
+            console.log('Doslo je do greske.')
+            console.error(err)
+            res.locals.message = err.message
+            res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+            // render the error page
+            res.status(500).render('error', { err })
+        }
+
     });
 
 }
@@ -112,6 +130,62 @@ exports.getAvgAllLastHour = (req, res) => {
     }
 }
 
+exports.getAvgAllLastDay = async (req, res) => {
+    const jsonData = [];
+    try {
+        for (i=1; i<25; i++){
+            let date1 = new Date();
+            date1.setHours(date1.getHours() - i);
+            let date2 = new Date();
+            date2.setHours(date2.getHours() - (i+1));
+
+            await Data.find({"date": {$gte: date1, $lt: date2}}, (err, data) => {
+                console.log(date1);
+                console.log(date2);
+                if (err) {
+                    console.error(err);
+                    res.status(501).send(err);
+                }else {
+                    let avgt = 0;
+                    let avgh = 0;
+                    let avgp = 0;
+                    data.forEach(temp => {
+                        avgt = avgt + temp.temperature;
+                        avgh = avgh + temp.humidity;
+                        avgp = avgp + temp.pressure;
+                    })
+                    avgt = avgt / data.length;
+                    avgh = avgh / data.length;
+                    avgp = avgp / data.length;
+                    let avg = {
+                        "temperature" : null,
+                        "humidity" : null,
+                        "pressure" : null,
+                        "date" : date2.toLocaleString('hr-HR')
+                    }
+                    if(data.length>0){
+                        avg = {
+                            "temperature" : avgt.toFixed(2),
+                            "humidity" : avgh.toFixed(2),
+                            "pressure" : avgp.toFixed(2),
+                            "date" : date2.toLocaleString('hr-HR')
+                        }
+                    }
+
+                    jsonData.push(avg);
+                    console.log(jsonData);
+                }
+            });
+        }
+        res.status(200).json(jsonData);
+
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+}
 
 exports.getAvgTempBtw = (req, res) => {
     try {
--- public/intro.js
+++ public/intro.js
@@ -5,61 +5,61 @@
  * Written by Nikola Kujaca <www.bdslab.info>,          *
  * nikola.kujaca@gmail.com                              *
  ********************************************************/
- (function(){
-   'use with caution :D'
-   let nikola = new Object();
-   let ver = 'NikolaKujaca v.44.0.0';
-   nikola.add(ver);
-    /* ******************************************************
-    * Loading all required middleware
-    ********************************************************/
-   let married = require('Andreja');
-   let children = married('Eleonora, Matej, Dejan');
-
-   nikola.get(married).have(children);
-
-   let spearfishing = require('sea');
-   let skiing = require('cold, snow, mountain')
-   let hobbies = require('freeTime');
-   hobbies.create(spearfishing, skiing);
-
-   try {
-     nikola.does(spearfishing().skiing());
-   }
-   catch(err) {
-       alert('No freeTime left!!!');
-   }
-
-
-   let javascript, web, spring, MySQL, android, ... = require('learning').Training();
-
-   let knowledge = new SkillSet();
-   function getKnowlegde(knowledge) {
-     do {
-       knowledge.add('AngularJS, Node.js, Express.js, socket.io...')(javascript);
-       knowledge.add('Security, MVC, JPA, Thymeleaf...')(spring);
-       knowledge.add('html5, css...')(web);
-       knowledge.add('GPS, scanner, service, xml...')(android);
-       ...
-     }
-     while (not_tired);
-     return knowledge;
-   }
-   ...
-   getKnowlegde(knowledge);
-
-   var examples = new array();
-   var project;
-   getKnowlegde(knowledge);
-   examples.add(project.use(knowledge.javascript(), 'http://www.worldsword.com'));
-   getKnowlegde(knowledge);
-   examples.add(project.use(knowledge.web(), 'http://www.aidascoaching.com'));
-   getKnowlegde(knowledge);
-   examples.add(project.use(knowledge.web(), 'http://www.bdslab.info'));
-   getKnowlegde(knowledge);
-   examples.add(project.use(knowledge.android(), 'https://play.google.com/store/apps/developer?id=Nikola+Kujaca'));
-   getKnowlegde(knowledge);
-   ...
+ // (function(){
+ //   'use with caution :D'
+ //   let nikola = new Object();
+ //   let ver = 'NikolaKujaca v.44.0.0';
+ //   nikola.add(ver);
+ //    /* ******************************************************
+ //    * Loading all required middleware
+ //    ********************************************************/
+ //   let married = require('Andreja');
+ //   let children = married('Eleonora, Matej, Dejan');
+ //
+ //   nikola.get(married).have(children);
+ //
+ //   let spearfishing = require('sea');
+ //   let skiing = require('cold, snow, mountain')
+ //   let hobbies = require('freeTime');
+ //   hobbies.create(spearfishing, skiing);
+ //
+ //   try {
+ //     nikola.does(spearfishing().skiing());
+ //   }
+ //   catch(err) {
+ //       alert('No freeTime left!!!');
+ //   }
+ //
+ //
+ //   let javascript, web, spring, MySQL, android, ... = require('learning').Training();
+ //
+ //   let knowledge = new SkillSet();
+ //   function getKnowlegde(knowledge) {
+ //     do {
+ //       knowledge.add('AngularJS, Node.js, Express.js, socket.io...')(javascript);
+ //       knowledge.add('Security, MVC, JPA, Thymeleaf...')(spring);
+ //       knowledge.add('html5, css...')(web);
+ //       knowledge.add('GPS, scanner, service, xml...')(android);
+ //       ...
+ //     }
+ //     while (not_tired);
+ //     return knowledge;
+ //   }
+ //   ...
+ //   getKnowlegde(knowledge);
+ //
+ //   var examples = new array();
+ //   var project;
+ //   getKnowlegde(knowledge);
+ //   examples.add(project.use(knowledge.javascript(), 'http://www.worldsword.com'));
+ //   getKnowlegde(knowledge);
+ //   examples.add(project.use(knowledge.web(), 'http://www.aidascoaching.com'));
+ //   getKnowlegde(knowledge);
+ //   examples.add(project.use(knowledge.web(), 'http://www.bdslab.info'));
+ //   getKnowlegde(knowledge);
+ //   examples.add(project.use(knowledge.android(), 'https://play.google.com/store/apps/developer?id=Nikola+Kujaca'));
+ //   getKnowlegde(knowledge);
+ //   ...
 
    /* ******************************************************
     * Routing and landing page
@@ -68,4 +68,4 @@
     * Also, permiting cross origin requests that are
     * required for image upload
     ********************************************************/
- }();
+ // }();
